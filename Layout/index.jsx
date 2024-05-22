607efb3cbc2e86daeb4057609bace2eaa0bb8cc9
izzatillonemataliev <156827918+izzatillonemataliev@users.noreply.github.com>
2024-05-22 22:58:18 +0500

    wck

---
+++ Layout/index.jsx
@@ -0,0 +1,31 @@
+
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import "./style.css";
+import Header from "../Pages/Header";
+
+function Layout() {
+
+     
+
+
+ 
+  return (
+    <>
+        <Header/>
+
+    </>
+  );
+}
+
+export default Layout;
+
+
+
+
+
+
+
+
+
+
